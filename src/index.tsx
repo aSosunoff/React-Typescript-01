69beb3df8690bb82a4e19f41d2b78d245c220bfe
asosunoff <al.sosunoff@gmail.com>
2021-01-05 16:58:11 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/app";
 import "materialize-css/dist/js/materialize.min.js";
 import "./assets/index.scss";
 
