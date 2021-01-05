a5fc20b0e6aa98ba4661771ca95c1497733176ef
asosunoff <al.sosunoff@gmail.com>
2021-01-05 16:54:34 +0300

    fix project

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "materialize-css/dist/js/materialize.min.js";
+import "./assets/index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
