32f37e2a4a0f91ede7a0384b793b56702dbeb7ac
asosunoff <al.sosunoff@gmail.com>
2021-01-05 17:26:27 +0300

    fix todo form

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoForm: React.FC = () => {
+  const [title, setTitle] = useState("");
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const keyPressHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      console.log(title);
+    }
+  };
+
   return (
     <div className="input-field col s6 mt2">
-      <input id="title" type="text" />
+      <input
+        id="title"
+        type="text"
+        value={title}
+        onChange={changeHandler}
+        onKeyPress={keyPressHandler}
+      />
       <label htmlFor="title">Введите название дела</label>
     </div>
   );
