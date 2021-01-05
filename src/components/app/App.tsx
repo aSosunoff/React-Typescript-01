c2bab69941c45458a4e396d3b52dc9f9f4ca60c7
asosunoff <al.sosunoff@gmail.com>
2021-01-05 17:08:59 +0300

    Fix app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,9 +5,7 @@ const App: React.FC = () => {
   return (
     <>
       <Navbar />
-      <div className="container">
-        <h1>Test</h1>
-      </div>
+      <div className="container"></div>
     </>
   );
 };
