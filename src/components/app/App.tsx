aa913396662d5f00a8ecbb30a9b7cfd6568201b0
asosunoff <al.sosunoff@gmail.com>
2021-01-05 18:13:47 +0300

    Added to app todo list component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
+import { ITodo } from "../interfaces";
 import Navbar from "../navbar";
 import TodoForm from "../TodoForm";
+import TodoList from "../TodoList";
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<Array<string>>([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
-  const addHandler = (title: string) => {
-    setTodos((prev) => [...prev, title]);
+  const addHandler = (title: string) =>
+    setTodos((prev) => [
+      {
+        title,
+        id: Date.now(),
+        completed: false,
+      },
+      ...prev,
+    ]);
+
+  const toggleHandler = (id: number) => {
+    setTodos((prev) =>
+      prev.map((element) =>
+        element.id === id
+          ? {
+              ...element,
+              completed: !element.completed,
+            }
+          : element
+      )
+    );
+  };
+
+  const removeHandler = (id: number) => {
+    setTodos((prev) => prev.filter((element) => element.id !== id));
   };
 
   return (
@@ -14,6 +39,12 @@ const App: React.FC = () => {
       <Navbar />
       <div className="container">
         <TodoForm onAdd={addHandler} />
+
+        <TodoList
+          todos={todos}
+          onRemove={removeHandler}
+          onToggle={toggleHandler}
+        />
       </div>
     </>
   );
